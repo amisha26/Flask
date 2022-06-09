d0831170eeaad0ed0c35078922973c7f3e66c13c
amisha26 <amisha8299@gmail.com>
2022-06-10 00:20:02 +0530

    added signup feature

---
+++ login.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+user_db = {"amisha" : "123", "amrita" : "321"}
+
+@app.route('/signup', methods=['POST'])
+def user_signup():
+    data = request.get_json()
+    user, passd = data["user"], data["passd"]
+    if user in user_db:
+        return jsonify({"msg":"User already exists"})
+    user_db[user] = passd
+    print(user_db,"---> user db")
+    return jsonify({"msg":"signup successful"})
+
+        
+if __name__ == '__main__':
+    app.run(debug=True)
