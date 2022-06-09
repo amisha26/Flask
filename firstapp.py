82a4e51a2a22c37fc57378966b4778a48d75b6ed
amisha26 <amisha8299@gmail.com>
2022-06-10 00:08:42 +0530

    first flask app

--- firstapp.py
+++ firstapp.py
@@ -3,3 +3,9 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
+def returnHelloWorld():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
